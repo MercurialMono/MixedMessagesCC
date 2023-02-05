1c96eb054c1f80a47e7c6e6bafbb6904d1c173f7
Mercurial <bry.beteta@gmail.com>
2023-02-05 11:23:25 -0500

    Initial Upload

---
+++ base.js
@@ -0,0 +1,17 @@
+const firstLine = ['I believe in one thing', ];
+const secondLine = ['and one thing only', ];
+const thirdLine = ['the power of human will', ];
+
+const randomFirst = (arr) => {
+    ['All that glitters', 'A stopped clock']
+}
+
+const randomSecond = (arr) => {
+    ['is not', 'is right', ]
+}
+
+const randomThird = (arr) => {
+    ['gold.', 'twice a day.']
+}
+
+console.log(randomFirst() + ' ' + randomSecond() + ' ' + randomThird());
